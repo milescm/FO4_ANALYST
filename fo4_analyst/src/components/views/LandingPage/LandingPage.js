6ef554a3ecb50e744771947e398f8b8f079f5814
TaewanKim <ktww88@gmail.com>
2020-08-01 19:00:27 +0900

    initialize project using CRA

---
+++ fo4_analyst/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+function LandingPage(props) {
+    return (
+        <div style={{
+            display:'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <h2>시작 페이지</h2>
+        </div>
+    );
+}
+
+export default LandingPage;
